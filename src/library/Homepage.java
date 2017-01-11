b0730a9874c6ecfc5b7e498e762a4974ae0ebb40
Adam Becvar <becvar.adam666@gmail.com>
2017-01-11 10:26:31 +0100

    Initial commit

---
+++ src/library/Homepage.java
@@ -0,0 +1,20 @@
+package library;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by adam on 11/01/2017.
+ */
+public class Homepage extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
