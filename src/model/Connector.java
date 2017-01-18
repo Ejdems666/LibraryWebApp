6fceb7f058c8d25c70a0e3581996f5461a1caa93
Adam Becvar <becvar.adam666@gmail.com>
2017-01-18 15:13:55 +0100

    corrected connector

--- src/model/Connector.java
+++ src/model/Connector.java
@@ -14,24 +14,17 @@ public class Connector {
 
     private Connection conn = null;
 
-    private static final String IP = "wm62.wedos.net";
+    private static final String IP = "localhost";
     private static final int PORT = 3306;
-    private static final String DATABASE = "d73332_library";
-    private static final String USERNAME = "w73332_library";
-    private static final String PASSWORD = "KDCpsaxQ";
+    private static final String DATABASE = "cba_library";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
-    /*
+
      public Connector() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
         this.conn = DriverManager.getConnection(url, USERNAME, PASSWORD);
-    }
-    */
-    public Connector() throws Exception {
-        Class.forName("com.mysql.jdbc.Driver");
-        System.out.println("Connecting to database...");
-        //STEP 3: Open a connection
-        this.conn = DriverManager.getConnection(IP, USERNAME, PASSWORD);
     }
 
     public Connection getConnection() {
